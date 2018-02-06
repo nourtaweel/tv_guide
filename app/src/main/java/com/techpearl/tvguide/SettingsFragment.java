eab500deac729435e644dab8c238e0d677854e9a
nour taweel <nour.taweel@gmail.com>
2018-02-06 10:57:00 +0000

    Force sync if country preference is changed

--- app/src/main/java/com/techpearl/tvguide/SettingsFragment.java
+++ app/src/main/java/com/techpearl/tvguide/SettingsFragment.java
@@ -1,13 +1,36 @@
 package com.techpearl.tvguide;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+import com.techpearl.tvguide.sync.TvGuideSyncUtils;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
 
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        if(key.equals(getString(R.string.pref_country_code_key))){
+            TvGuideSyncUtils.startImmediateSync(getContext());
+        }
     }
 }
